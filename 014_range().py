17d25fbc0e02765682938d2f8253c4adf3ebf41c
serd4r <31065859+serd4r@users.noreply.github.com>
2021-10-30 20:07:53 +0300

    Add files via upload

---
+++ 014_range().py
@@ -0,0 +1,11 @@
+print(*range(1, 100))
+print("Aralıklı 1to250 : ")
+print(*range(0, 250, 2))
+print("Ters : ")
+print(*range(100, 0, -1))
+
+for i in range(0, 101):
+    print(i)
+
+for i in range(0, 10):
+    print("*" * i)
