3ae1f8b9167407a0d4074718db85e6f7d6e8bfc9
serd4r <31065859+serd4r@users.noreply.github.com>
2021-10-30 15:03:40 +0300

    Add files via upload

---
+++ 013_while.py
@@ -0,0 +1,20 @@
+i = 0
+
+while (i <= 10):
+    print(i)
+    i += 1
+
+# 20 ye kadar olan çift sayılar
+i = 0
+
+while(i <= 20):
+    print(i)
+    i += 2
+
+# liste üzerinde indeks ile gezinme
+liste = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+i = 0
+while (i < len(liste)):  # liste uzunluğu kadar döndür
+    print(liste[i])
+    i += 1
