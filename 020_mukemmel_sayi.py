8361fb2cd0feda7cb72e260234f283fe6d6c8ba6
serd4r <31065859+serd4r@users.noreply.github.com>
2021-11-07 11:36:09 +0300

    Add files via upload

---
+++ 020_mukemmel_sayi.py
@@ -0,0 +1,10 @@
+# mükemmel sayi kontrolü
+# @serd4ru
+
+sayi = int(input("Mükemmel mi ? : "))
+
+for i in range(1, sayi):
+    if (sayi % i == 0):
+        sayi
+    if (i + i == sayi):
+        print(sayi, "mükemmel sayi !")
